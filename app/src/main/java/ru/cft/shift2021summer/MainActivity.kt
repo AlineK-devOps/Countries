40272066fcb6511b9085dcc905e4e5d1744e33a3
Aline Korshunova <aline.korshunova00@gmail.com>
2021-07-05 01:53:41 +0300

    Added data to the list

--- app/src/main/java/ru/cft/shift2021summer/MainActivity.kt
+++ app/src/main/java/ru/cft/shift2021summer/MainActivity.kt
@@ -2,19 +2,31 @@ package ru.cft.shift2021summer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import ru.cft.shift2021summer.adapters.CountriesAdapter
 import ru.cft.shift2021summer.testdata.CountryRepository
 
 class MainActivity : AppCompatActivity() {
     private lateinit var countryRepository: CountryRepository
     private lateinit var countriesRecycler: RecyclerView
+    private val adapter = CountriesAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         countriesRecycler = findViewById(R.id.countriesRecycler)
+        countriesRecycler.adapter = adapter
+        countriesRecycler.layoutManager = LinearLayoutManager(this)
+
         countryRepository = CountryRepository()
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        adapter.countries = countryRepository.getAllCountries()
     }
 }
