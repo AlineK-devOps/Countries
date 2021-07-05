83b807e17d959a20fc17c8e05097f67fdc50845c
Aline Korshunova <aline.korshunova00@gmail.com>
2021-07-05 11:44:34 +0300

    Added screen switching after 3 seconds

--- app/src/main/java/ru/cft/shift2021summer/SplashScreenActivity.kt
+++ app/src/main/java/ru/cft/shift2021summer/SplashScreenActivity.kt
@@ -3,7 +3,21 @@ package ru.cft.shift2021summer
 /** Сплэшскрин - заставка приложения перед запуском **/
 
 import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
 
 class SplashScreenActivity : Activity() {
+    private val SPLASH_DISPLAY_LENGTH: Long = 3000 //отображение сплэшскрина 3 секунды
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler().postDelayed({ //переключение экрана
+            val mainIntent = Intent(this, MainActivity::class.java)
+            this.startActivity(mainIntent)
+            this.finish()
+        }, SPLASH_DISPLAY_LENGTH)
+    }
 }
--- app/src/main/java/ru/cft/shift2021summer/testdata/CountryRepository.kt
+++ app/src/main/java/ru/cft/shift2021summer/testdata/CountryRepository.kt
@@ -7,71 +7,51 @@ class CountryRepository {
         CountrySimplified(
           name = "Russia",
           capital = "Moscow",
-          population = 146_238_185,
-          area = 17_125_191.0
         ),
 
         CountrySimplified(
             name = "Canada",
             capital = "Ottawa",
-            population = 38_048_738,
-            area = 9_984_670.0
         ),
 
         CountrySimplified(
             name = "USA",
             capital = "Washington",
-            population = 331_449_281,
-            area = 3_796_742.0
         ),
 
         CountrySimplified(
             name = "France",
             capital = "Paris",
-            population = 67_413_000,
-            area = 640_679.0
         ),
 
         CountrySimplified(
             name = "Mexico",
             capital = "Mexico City",
-            population = 126_014_024,
-            area = 1_972_550.0
         ),
 
         CountrySimplified(
             name = "Germany",
             capital = "Berlin",
-            population = 83_190_556,
-            area = 357_022.0
         ),
 
         CountrySimplified(
             name = "Netherlands",
             capital = "Amsterdam",
-            population = 17_611_300,
-            area = 41_865.0
         ),
 
         CountrySimplified(
             name = "Italy",
             capital = "Rome",
-            population = 60_317_116,
-            area = 301_340.0
         ),
 
         CountrySimplified(
             name = "Ukraine",
             capital = "Kyiv",
-            population = 41_442_615,
-            area = 603_628.0
         ),
 
         CountrySimplified(
             name = "Norway",
             capital = "Oslo",
-            population = 5_391_369,
-            area = 385_207.0
         )
     )
 
