d143bc78a09d599bf2f3c2631d8a993310e18ac2
Aline Korshunova <aline.korshunova00@gmail.com>
2021-07-06 21:01:53 +0300

    Implemented CountryDetailsActivity

--- app/src/main/java/ru/cft/shift2021summer/CountryDetailsActivity.kt
+++ app/src/main/java/ru/cft/shift2021summer/CountryDetailsActivity.kt
@@ -3,19 +3,23 @@ package ru.cft.shift2021summer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import ru.cft.shift2021summer.testdata.CountryRepository
 import ru.cft.shift2021summer.testdata.CountrySimplified
 
 class CountryDetailsActivity : AppCompatActivity() {
+    private lateinit var countriesRepository: CountryRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_country_details)
 
+        countriesRepository = CountryRepository()
+        val country = countriesRepository.getCountry("Russia")
+
         val nameText = findViewById<TextView>(R.id.nameText)
         val infoText = findViewById<TextView>(R.id.infoText)
 
-        val country = CountrySimplified("Russia", "Moscow")
-
-        nameText.text = getString(R.string.name_country_format,country.name)
-        infoText.text = getString(R.string.info_format,country.capital)
+        nameText.text = getString(R.string.name_country_format,country?.name)
+        infoText.text = getString(R.string.info_format,country?.capital)
     }
 }
