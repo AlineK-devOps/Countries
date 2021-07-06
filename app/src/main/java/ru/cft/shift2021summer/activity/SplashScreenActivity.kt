0cc03d4908ced3576c0a4cc65452ddf019b995cd
Aline Korshunova <aline.korshunova00@gmail.com>
2021-07-07 02:49:17 +0300

    Redesign splash screen

--- app/src/main/java/ru/cft/shift2021summer/activity/SplashScreenActivity.kt
+++ app/src/main/java/ru/cft/shift2021summer/activity/SplashScreenActivity.kt
@@ -4,8 +4,14 @@ package ru.cft.shift2021summer.activity
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.view.View
+import android.widget.TextView
 import ru.cft.shift2021summer.R
 
 class SplashScreenActivity : Activity() {
@@ -16,6 +22,12 @@ class SplashScreenActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+
+        val textview = findViewById<View>(R.id.textView) as TextView
+        val spans: Spannable = SpannableString("WORLDWIDE")
+        spans.setSpan(ForegroundColorSpan(Color.parseColor("#304d71")), 0, 5, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spans.setSpan(ForegroundColorSpan(Color.parseColor("#5baee4")), 5, 9, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textview.text = spans
 
         Handler().postDelayed({ //переключение экрана
             val mainIntent = Intent(this, MainActivity::class.java)
