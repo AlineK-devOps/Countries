acbb2067dc78a1f94689e1ad8afff5023c7492bf
Aline Korshunova <aline.korshunova00@gmail.com>
2021-07-13 20:42:49 +0300

    Design filter view

--- app/src/main/java/ru/cft/shift2021summer/countries/filter/presentation/FilterPresenter.kt
+++ app/src/main/java/ru/cft/shift2021summer/countries/filter/presentation/FilterPresenter.kt
@@ -1,9 +1,6 @@
 package ru.cft.shift2021summer.countries.filter.presentation
 
-import io.reactivex.android.schedulers.AndroidSchedulers
 import ru.cft.shift2021summer.countries.base.BasePresenter
-import java.util.*
-import kotlin.random.Random
 
 class FilterPresenter: BasePresenter<FilterView>() {
 
