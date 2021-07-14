a2050cd821652d1303577f21d22036bf6c518934
Aline Korshunova <aline.korshunova00@gmail.com>
2021-07-14 11:56:08 +0300

    Added filtering of the list by regions and regional blocks

--- app/src/main/java/ru/cft/shift2021summer/countries/main/presentation/MainPresenter.kt
+++ app/src/main/java/ru/cft/shift2021summer/countries/main/presentation/MainPresenter.kt
@@ -23,8 +23,49 @@ class  MainPresenter(
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeBy(
                 onSuccess = {
-                    //сортировка списка
-                    view?.bindCountry(it)
+
+                    if (params != null && params.isNotEmpty()){
+                        var filteredCountries: MutableList<CountryModel> = mutableListOf()
+                        var regions: MutableList<String> = mutableListOf()
+                        var regionalBlocks: MutableList<String> = mutableListOf()
+
+                        for (str in params){
+                            val list: List<String> = str.split("-")
+                            if (list[0] == "region")
+                                regions.add(list[1].lowercase())
+                            else
+                                regionalBlocks.add(list[1].lowercase())
+                        }
+
+                        for (country in it){
+                            var isCorrectRegion = false
+                            var isCorrectBlock = false
+
+                            if (regions.isNotEmpty()){
+                                if (regions.contains("all") || regions.contains(country.region.lowercase())){
+                                    isCorrectRegion = true
+                                }
+                            }
+                            else isCorrectRegion = true
+
+                            if (regionalBlocks.isNotEmpty()){
+                                for (block in country.regionalBlocs){
+                                    if (regionalBlocks.contains("all") || regionalBlocks.contains(block.acronym.lowercase())){
+                                        isCorrectBlock = true
+                                        break
+                                    }
+                                }
+                            }
+                            else isCorrectBlock = true
+
+                            if (isCorrectRegion && isCorrectBlock)
+                                filteredCountries.add(country)
+                        }
+
+                        view?.bindCountry(filteredCountries)
+                    }
+                    else view?.bindCountry(it)
+
                     view?.bindRandomCountry(it[Random.nextInt(0, it.size - 1)])
                 },
             )
